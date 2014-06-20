780407d24e5d6e8f6c3e17d14cdf5ec2959fd443
1xristos2 <xristoskonsta@gmail.com>
2014-06-20 11:14:55 +0000

    first commit
    nothing added

--- wp-activate.php
+++ wp-activate.php
@@ -10,7 +10,7 @@ define( 'WP_INSTALLING', true );
 
 /** Sets up the WordPress Environment. */
 require( dirname(__FILE__) . '/wp-load.php' );
-
+ 
 require( dirname( __FILE__ ) . '/wp-blog-header.php' );
 
 if ( !is_multisite() ) {
